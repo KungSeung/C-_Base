303e6f19fef6d14ea26af1fba58c869cf4c4e09e
KungSeung <nkhone123456@naver.com>
2022-01-24 15:50:51 +0900

    Data Structure

---
+++ DataSturcture/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DataSturcture
+{
+
+	class Program
+	{
+
+		/*static void Main(string[] args)
+		{
+			// 배열
+			int[] scores = new int[5];  // 배열을 동적으로 할당 중 -> 참조 타입
+			int[] scores2 = scores;
+
+			scores2[0] = 9999;	// 원본이 변경되는 것을 확인가능
+
+			// 0 1 2 3 4
+			scores[0] = 10;
+			scores[1] = 20;
+			scores[2] = 30;
+			scores[3] = 40;
+			scores[4] = 50;
+
+			foreach(int score in scores)
+			{
+				Console.WriteLine(score);		// cww Tab Tab
+			}
+		}*/
+	}
+}
---
+++ DataSturcture/Program2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataSturcture
+{
+	class Player
+	{
+
+	}
+	class Monster
+	{
+
+	}
+
+	class Program2
+	{
+		static int GetHighestScore(int[] scores)
+		{
+			int max = scores[0];
+			foreach (int score in scores) {
+				if (max < score)
+					max = score;
+			}
+			return max;
+		}
+
+		static int GetAverageScore(int[] scores)
+		{
+			int Average = 0;
+			foreach(int score in scores){ Average += score; }
+			if (scores.Length == 0)
+				return 0;
+			return Average / scores.Length;
+		}
+
+		static int GetIndexOf(int[] scores, int value)
+		{
+			int index = 0;
+			foreach(int score in scores){
+				if (value == score){ return index; }
+				index++;
+			}
+			return -1;
+		}
+
+		static void Sort(int[] scores)
+		{
+			// 정렬 : 오름차순
+			int temp = 0;
+			for(int i=0; i < scores.Length-1; i++)		// i : 0 1 2 3 / j : 1 2 3 4 
+			{
+				int min = scores[i];
+				for(int j = i + 1; j < scores.Length; j++)		
+				{
+					if (min > scores[j])      
+					{
+						temp = scores[j];
+						scores[j] = min;
+						scores[i] = temp;
+					}
+				}
+			}
+
+			for(int i=0; i<scores.Length; i++)
+			{
+				// [i ~ scores.Length - 1] 제일 작은 숫자가 있는 index찾기
+				int minIndex = i;
+				for(int j = i; j < scores.Length; j++)
+				{
+					if (scores[j] < scores[minIndex])
+						minIndex = j;
+				}
+
+				// swap
+				int tmp = scores[i];
+				scores[i] = scores[minIndex];
+				scores[minIndex] = tmp;
+			}
+
+		}
+
+		static void Main(string[] args)
+		{
+			int[] scores = new int[5] { 10, 30, 40, 20, 50 };
+			Sort(scores);
+			foreach(int score in scores)
+			{
+				Console.Write(score);
+			}
+		}
+	}
+}
